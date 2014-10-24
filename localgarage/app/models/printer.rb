3b4302175ac9a69602abf5aa1094e9e69832cf1e
masa321 <mitsuimasayoshi@gmail.com>
2014-10-24 19:26:11 +0900

    from validation

--- localgarage/app/models/printer.rb
+++ localgarage/app/models/printer.rb
@@ -1,8 +1,10 @@
 class Printer < ActiveRecord::Base
  # belongs_to :user
+  validates :address, presence: true
   has_and_belongs_to_many :filaments
   geocoded_by :address
   after_validation :geocode
+
 
   def self.search(search)
     if search
