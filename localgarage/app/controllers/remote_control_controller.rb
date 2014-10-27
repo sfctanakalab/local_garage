b873c795d668241641f2723cf483a7356642d4e8
klee-arc <kenfjy@gmail.com>
2014-10-28 08:45:52 +0900

    remote control sender complete

--- localgarage/app/controllers/remote_control_controller.rb
+++ localgarage/app/controllers/remote_control_controller.rb
@@ -4,15 +4,16 @@ class RemoteControlController < WebsocketRails::BaseController
   # status_to_user => status_update
   def load_status
     res = message()
+    Rails.logger.debug "load_status: #{res}"
     WebsocketRails[:printer_status].trigger(:status_update, res)
   end
 
   # user_command => command_to_printer
   def load_command
     res = message()
-    # Rails.logger.debug "reached message: #{res}"
+    Rails.logger.debug "load_command: #{res}"
 
-    broadcast_message(:print_command, res, :namespace => "lg_to_user")
+    broadcast_message(:print_command, res, :namespace => "lg_to_octo")
     WebsocketRails[:printer_status].trigger(:sent_command, res)
   end
 
