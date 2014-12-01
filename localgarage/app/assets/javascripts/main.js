92afa5fdfabc61619bf9dd5d3c3e552bb73ee028
Moriya Takumi <ta9quzomri1ya@gmail.com>
2014-12-01 14:48:14 +0900

    landing page design

--- localgarage/app/assets/javascripts/main.js
+++ localgarage/app/assets/javascripts/main.js
@@ -19,3 +19,24 @@ function closeNav() {
 	var navlink = document.getElementById('navlink');
 	navlink.style.display = "none";
 }
+var linkTouchStart = function(){
+    thisAnchor = $(this);
+    touchPos = thisAnchor.offset().top;
+    moveCheck = function(){
+        nowPos = thisAnchor.offset().top;
+        if(touchPos == nowPos){
+            thisAnchor.addClass("hover");
+        }
+    }
+    setTimeout(moveCheck,100);
+}
+var linkTouchEnd = function(){
+    thisAnchor = $(this);
+    hoverRemove = function(){
+        thisAnchor.removeClass("hover");
+    }
+    setTimeout(hoverRemove,500);
+}
+ 
+$(document).on('touchstart mousedown','a',linkTouchStart);
+$(document).on('touchend mouseup','a',linkTouchEnd);
